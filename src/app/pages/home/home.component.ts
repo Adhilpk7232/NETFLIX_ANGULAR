daa8c507ec9ed5eb48b337e551b86917cfc8b780
Adhilpk7232 <adhilpk12@gmail.com>
2023-06-07 10:41:24 +0530

    almost complete

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+import { tick } from '@angular/core/testing';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+
+  constructor(private service:MovieApiServiceService){}
+  
+  bannerResult:any=[];
+  trendingMovieResult:any=[]
+  actionMovieResult:any=[]
+  adventureMovieResult: any = [];
+  animationMovieResult: any = [];
+  comedyMovieResult: any = [];
+  documentaryMovieResult: any = [];
+  scienceFictionMovieResult: any = [];
+  thrillerMovieResult: any = [];
+  
+  ngOnInit(): void {
+    this.bannerData();
+    this.trendingData();
+    this.actionMovie();
+    this.adventureMovie();
+    this.animationMovie();
+    this.comedyMovie();
+    this.documentaryMovie();
+    this.scienceFictionMovie();
+    this.thrillerMovie();
+  }
+  // bannerApiData
+  bannerData(){
+    this.service.bannerApiData().subscribe((result)=>{
+      console.log(result,'bannerresult#');
+      this.bannerResult = result.results;
+      
+    })
+  }
+
+  trendingData(){
+    this.service.trendingMovieApiData().subscribe((result) => { 
+      console.log(result,'trendingresult#');
+      this.trendingMovieResult = result.results;
+      
+    })
+  }
+  // action
+  actionMovie() {
+    this.service.fetchActionMovies().subscribe((result) => {
+      // console.log(result,'actionMovie#');
+      this.actionMovieResult = result.results;
+    });
+  }
+  // adventure
+  adventureMovie() {
+    this.service.fetchAdventureMovies().subscribe((result) => {
+      // console.log(result,'actionMovie#');
+      this.adventureMovieResult = result.results;
+    });
+  } // animation
+  animationMovie() {
+    this.service.fetchAnimationMovies().subscribe((result) => {
+      // console.log(result,'actionMovie#');
+      this.animationMovieResult = result.results;
+    });
+  } // comedy
+  comedyMovie() {
+    this.service.fetchComedyMovies().subscribe((result) => {
+      // console.log(result,'actionMovie#');
+      this.comedyMovieResult = result.results;
+    });
+  } // documentary
+  documentaryMovie() {
+    this.service.fetchDocumentaryMovies().subscribe((result) => {
+      // console.log(result,'actionMovie#');
+      this.documentaryMovieResult = result.results;
+    });
+  } // scienceFiction
+  scienceFictionMovie() {
+    this.service.fetchScienceFictionMovies().subscribe((result) => {
+      // console.log(result,'actionMovie#');
+      this.scienceFictionMovieResult = result.results;
+    });
+  } // thriller
+  thrillerMovie() {
+    this.service.fetchThrillerMovies().subscribe((result) => {
+      // console.log(result,'actionMovie#');
+      this.thrillerMovieResult = result.results;
+    });
+  }
+}
+
